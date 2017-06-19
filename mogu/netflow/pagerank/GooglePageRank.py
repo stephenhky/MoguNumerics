ceddd1dd84d7010d2d38cbc130f8c5e32c7c22f2
stephenhky <stephenhky@yahoo.com.hk>
2017-06-18 21:30:44 -0400

    started to implement pagerank with fortran90

--- mogu/netflow/pagerank/GooglePageRank.py
+++ mogu/netflow/pagerank/GooglePageRank.py
@@ -3,9 +3,7 @@ import numpy as np
 
 
 def GoogleMatrix(digraph, beta):
-    nodedict = {}
-    for idx, node in zip(range(len(digraph)), digraph.nodes()):
-        nodedict[node] = idx
+    nodedict = {node: idx for idx, node in zip(range(len(digraph)), digraph.nodes())}
     A = np.matrix((1 - beta) / float(len(digraph)) * np.ones(shape=(len(digraph), len(digraph))))
     for node1, node2 in digraph.edges():
         A[nodedict[node2], nodedict[node1]] += beta / float(len(digraph.successors(node1)))
@@ -16,17 +14,20 @@ def L1norm(r1, r2):
     return np.sum(abs(r1 - r2))
 
 
-def CalculatePageRank(digraph, beta, eps=1e-4, maxstep=1000):
+def CalculatePageRank(digraph, beta, eps=1e-4, maxstep=1000, fortran=False):
     A, nodes = GoogleMatrix(digraph, beta)
     r = np.transpose(np.matrix(np.repeat(1 / float(len(digraph)), len(digraph))))
-    converged = False
-    stepid = 0
-    while not converged and stepid < maxstep:
-        newr = A * r
-        converged = (L1norm(newr, r) < eps)
-        r = newr
-        stepid += 1
-    nodepr = {}
-    for node in nodes:
-        nodepr[node] = r[nodes[node], 0]
-    return nodepr
+    if fortran:
+        pass
+    else:
+        converged = False
+        stepid = 0
+        while not converged and stepid < maxstep:
+            newr = A * r
+            converged = (L1norm(newr, r) < eps)
+            r = newr
+            stepid += 1
+        nodepr = {}
+        for node in nodes:
+            nodepr[node] = r[nodes[node], 0]
+        return nodepr
---
+++ mogu/netflow/pagerank/pr_markovmatrix.f90
@@ -0,0 +1,15 @@
+module pr_markovmatrix
+  implicit none
+  private
+  public
+
+contains
+
+  function pagerank_markovmatrix(A, r) result(finalr)
+    real, dimension(:, :) :: A
+    real, dimension(:) :: r
+
+  end function pagerank_markovmatrix
+
+end module pr_markovmatrix
+
