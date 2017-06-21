a8a045b3255c43289e919253144460cc0ece3de4
stephenhky <stephenhky@yahoo.com.hk>
2017-06-21 13:15:17 -0400

    corrected C code for damerau-levenshtein distance

--- mogu/dynprog/dldist.c
+++ mogu/dynprog/dldist.c
@@ -23,17 +23,23 @@ int damerau_levenshtein(char *word1, char *word2)
       int subcost = matrix[i-1][j-1] + cost;
       int score = MIN(MIN(delcost, inscost), subcost);
       if ((i>1) & (j>1) & (word1[i]==word2[j-1]) & (word1[i-1]==word2[j])) {
-	score = MIN(matrix[i][j], matrix[i-2][j-2]+cost);
+	    score = MIN(score, matrix[i-2][j-2]+cost);
       }
       matrix[i][j] = score;
     }
   }
-//  for (int i=0; i<=len1; i++) for (int j=0; j<=len2; j++) {
-//      printf("%i %i %i\n", i, j, matrix[i][j]);
+//  for (int i=0; i<=len1; i++) {
+//    for (int j=0; j<=len2; j++) {
+//      printf("%i ", matrix[i][j]);
 //    }
+//    printf("\n");
+//  }
   return(matrix[len1][len2]);
 }
 
-int main() {
-  printf("driver diver %i\n", damerau_levenshtein("driver", "rdvie"));
+int main(int numargs, char **words) {
+  char *word1 = words[1];
+  char *word2 = words[2];
+  printf("%s %s %i\n", word1, word2, damerau_levenshtein(word1, word2));
+  return(0);
 }
