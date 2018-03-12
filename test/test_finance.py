34ae1d2f0ae8c448cde52f49885547567106d871
stephenhky <stephenhky@yahoo.com.hk>
2018-03-12 12:38:40 -0400

    updated unit test for binomial tree

--- test/test_finance.py
+++ test/test_finance.py
@@ -1,7 +1,7 @@
 
 import unittest
 
-from mogu.finance.binomial import eurocall_price
+from mogu.finance.binomial import eurocall_price, europut_price, amcall_price, amput_price
 
 class test_finance(unittest.TestCase):
     def setUp(self):
@@ -11,9 +11,25 @@ class test_finance(unittest.TestCase):
         pass
 
     def testEuroCall(self):
-        self.assertAlmostEquals(eurocall_price(100, 110, 0.01, 0.01, 10, 10000),
+        self.assertAlmostEquals(eurocall_price(100.0, 110.0, 0.01, 0.01, 10.0, 10000),
                                 1.4968152046203613,
                                 delta=1e-4)
+
+    def testEuroPut(self):
+        self.assertAlmostEquals(europut_price(100.0, 80.0, 0.05, 0.05, 10.0, 10000),
+                                5.284386951732304e-06,
+                                delta=1e-6)
+
+    def testAmCall(self):
+        self.assertAlmostEquals(amcall_price(100.0, 110.0, 0.05, 0.05, 10.0, 10000),
+                                33.303010096394594,
+                                delta=1e-3)
+
+    def testAmPut(self):
+        self.assertAlmostEquals(amput_price(100.0, 80.0, 0.05, 0.05, 10.0, 10000),
+                                9.38288088691752e-05,
+                                delta=1e-6)
+
 
 if __name__ == '__main__':
     unittest.main()
