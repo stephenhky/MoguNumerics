7b9e958c832804e6d17238f34ab169abb7f0fcbb
stephenhky <stephenhky@yahoo.com.hk>
2018-07-25 13:08:10 -0400

    updated version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def readme():
 
 
 setup(name='mogu',
-      version="0.2.1a0",
+      version="0.2.1a1",
       description="Collection of Simple Numerical Routines",
       long_description="Collection of simple numerical routines, independent of each other",
       classifiers=[
