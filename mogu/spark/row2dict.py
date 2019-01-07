ef8794abd6e9815f0fd1df98d8e78763c42628fa
stephenhky <stephenhky@yahoo.com.hk>
2019-01-07 18:43:56 -0500

    bug fix for empty list

--- mogu/spark/row2dict.py
+++ mogu/spark/row2dict.py
@@ -7,7 +7,7 @@ def convertRowToDict(row):
     for key in targetdict:
         if (targetdict[key])==Row:
             targetdict[key] = convertRowToDict(targetdict[key])
-        elif type(targetdict[key])==list and type(targetdict[key][0])==Row:
+        elif type(targetdict[key])==list and len(targetdict[key])>0 and type(targetdict[key][0])==Row:
             targetdict[key] = convertRowsToDict(targetdict[key])
     return targetdict
 
@@ -16,7 +16,7 @@ def convertRowsToDict(rows):
     targetdicts = [row.asDict() for row in rows if row is not None]
     for targetdict in targetdicts:
         for key in targetdict:
-            if type(targetdict[key])==list and type(targetdict[key][0])==Row:
+            if type(targetdict[key])==list and len(targetdict[key])>0 and type(targetdict[key][0])==Row:
                 targetdict[key] = convertRowsToDict(targetdict[key])
             if type(targetdict[key])==Row:
                 targetdict[key] = convertRowToDict(targetdict[key])
