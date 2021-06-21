10c3732c3558eee4d7c21863fbfb76f9ffa51aa2
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-06-21 18:59:14 -0400

    using numpy sum

--- mogu/spectral/dft.py
+++ mogu/spectral/dft.py
@@ -8,7 +8,7 @@ def slowDFT(t):
     N = t.shape[0]
     omega = np.zeros(N, dtype=np.complex_)
     for n in range(N):
-        omega[n] = sum(t[m] * np.exp(-1j*2*np.pi*m*n/N) for m in range(N))
+        omega[n] = np.sum(t[m] * np.exp(-1j*2*np.pi*m*n/N) for m in range(N))
     return omega
 
 
@@ -17,5 +17,5 @@ def slowInvDFT(omega):
     N = omega.shape[0]
     t = np.zeros(N, dtype=np.complex_)
     for m in range(N):
-        t[m] = sum(omega[n] * np.exp(1j*2*np.pi*m*n/N) for n in range(N)) / N
+        t[m] = np.sum(omega[n] * np.exp(1j*2*np.pi*m*n/N) for n in range(N)) / N
     return t
