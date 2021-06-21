b338f2c69dcc86184121d6a3a7189038d19124bb
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-06-21 19:05:00 -0400

    not using numba

--- mogu/spectral/fft.py
+++ mogu/spectral/fft.py
@@ -1,9 +1,7 @@
 
 import numpy as np
-import numba as nb
 
 
-@nb.njit(nb.complex64[:](nb.complex64[:]))
 def FFT(f):
     nbdata = f.shape[0]
     assert np.log(nbdata) / np.log(2) % 1 == 0.0
@@ -23,7 +21,6 @@ def FFT(f):
     return g
 
 
-@nb.njit(nb.complex64[:](nb.complex64[:]))
 def invFFTrecursive(f):
     nbdata = f.shape[0]
     assert np.log(nbdata) / np.log(2) % 1 == 0.0
