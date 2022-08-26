a522b37fea033d1bc3fdbfdfce204cff8c9bed81
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-08-26 19:16:18 -0400

    initiating the development of Lanzco's algorithm

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def install_requirements():
 
 
 setup(name='mogu',
-      version="0.7.0",
+      version="0.8.0a1",
       description="Collection of Simple Numerical Routines",
       long_description="Collection of simple numerical routines, independent of each other",
       classifiers=[
